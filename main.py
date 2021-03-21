1eba789dc241c8bba9819612413583a0237ece29
Your Name <you@example.com>
2021-03-21 03:38:38 -0500

    added testing on_message event

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from dotenv import load_dotenv
+load_dotenv()
 import discord
 import os
-
 
 intents = discord.Intents.default()
 intents.members = True
@@ -9,7 +10,6 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_member_join(member):
- # APPLICANT_TABLE[member.id].append(APPLICANT_DATA)
   print('Member joined, sending application')
   if member.dm_channel:
     return
@@ -17,6 +17,13 @@ async def on_member_join(member):
   await member.create_dm() 
   await member.dm_channel.send('IGN:')
 
-#ss
+#Using this event to test the dm message without new person joining the discord
+@client.event
+async def on_message(message):
+  if message.author.id is client.user.id:
+    return
+  if message.content.startswith('!porn'):
+    await message.author.create_dm()
+    await message.author.dm_channel.send('Hey! Welcome to Lasagn :tada:! Please read the rules in the rules channel, and copy paste your application into this bot\'s dms!')
 
-client.run(os.getenv('TOKEN'))
+client.run(os.getenv("TOKEN"))
